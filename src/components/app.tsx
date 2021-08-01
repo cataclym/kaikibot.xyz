aa13b8c0f7024eb02639eec6691ee916d79ee833
Cata <11278743+cataclym@users.noreply.github.com>
2021-08-01 02:16:48 +0200

    v0.1.0

---
+++ src/components/app.tsx
@@ -0,0 +1,128 @@
+import React, { Component, useState } from "react";
+import "../css/styles.css";
+import Commands from "./commands";
+import embedIcon from "../images/streamline-coding-search--coding--400x400.png";
+import discordIcon from "../images/streamline-icon-customer-server-imac-4@400x400.png";
+import gitLabIcon from "../images/gitlab-icon-rgb_resized.png";
+import inviteIcon from "../images/streamline-avatar-neutral-add-2--users--400x400.png";
+import listIcon from "../images/streamline-invoice-spreadsheet-1--business--400x400.png";
+
+export default class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.handleCommandClick = this.handleCommandClick.bind(this);
+    this.state = {
+      toggle: false,
+    };
+  }
+
+  handleCommandClick() {
+    // @ts-ignore
+    this.setState((state) => ({ toggle: !state.toggle }));
+  }
+
+  images = [
+    "https://safebooru.org//images/2997/080802f151827f84ccb9c6d0dca78457aebd72bf.jpg",
+    "https://safebooru.org//images/2978/9d7a44378785c4863665c83bcfaede66f05d2599.jpg",
+    "https://safebooru.org//images/1437/3629c6ead1a28579bfd4ea7d7fa3b5c4dbb8f224.jpg",
+  ];
+
+  render() {
+    // @ts-ignore
+    const toggle = this.state.toggle;
+    let bg;
+    let cmdButton;
+
+    if (toggle) {
+      bg = React.createElement(Commands);
+      cmdButton = (
+        <a
+          className="button5 toggled"
+          href="#commands"
+          onClick={this.handleCommandClick}
+        >
+          Back
+          <img id="2" alt="" className="icon" src={listIcon} />
+        </a>
+      );
+    } else {
+      bg = (
+        <div className="bg">
+          <img alt="" src={this.randomImg()} />
+        </div>
+      );
+      cmdButton = (
+        <a
+          className="button5 white"
+          href="#commands"
+          onClick={this.handleCommandClick}
+        >
+          Commands
+          <img id="2" alt="" className="icon" src={listIcon} />
+        </a>
+      );
+    }
+    return (
+      <div>
+        <div className="header1">
+          <p className="p1S1">KAIKIBOT</p>
+          <p className="p1S2">Your dad isn't this cool</p>
+          <a
+            className="button5 white"
+            href="https://discord.com/oauth2/authorize?client_id=714695773534814238&scope=bot"
+          >
+            Invite
+            <img id="1" alt="" className="icon" src={inviteIcon} />
+          </a>
+          {cmdButton}
+          <a className="button5 white" href="https://embed.kaikibot.xyz/">
+            Embed builder
+            <img id="3" alt="" className="icon" src={embedIcon} />
+          </a>
+          <a className="button5 discord" href="https://embed.kaikibot.xyz/">
+            Support server
+            <img id="4" alt="" className="icon" src={discordIcon} />
+          </a>
+          <a
+            className="button5 gitlab"
+            href="https://gitlab.com/cataclym/KaikiDeishuBot"
+          >
+            GitLab
+            <img id="5" alt="" className="icon" src={gitLabIcon} />
+          </a>
+        </div>
+        {bg}
+        <p
+          style={{
+            alignContent: "center",
+            textAlign: "center",
+            margin: "auto",
+            marginTop: "1vw",
+            width: 100,
+            color: "white",
+          }}
+        >
+          © cata 2021
+        </p>
+        <p
+          style={{
+            alignContent: "center",
+            textAlign: "center",
+            margin: "auto",
+            marginTop: "1vw",
+            width: 400,
+            color: "white",
+          }}
+        >
+          <a href="https://www.streamlinehq.com">
+            Free vectors icons and illustrations from Streamline
+          </a>
+        </p>
+      </div>
+    );
+  }
+
+  randomImg() {
+    return this.images[Math.floor(Math.random() * this.images.length)];
+  }
+}
---
+++ src/components/commands.tsx
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import commandslist from "./cmdlist.json";
+import "../css/commands.css";
+
+export interface Command {
+  id: string;
+  categoryID: Categories;
+  category: string[];
+  filepath: string;
+  aliases: string[];
+  channel: Channel | null;
+  ownerOnly: boolean;
+  editable: boolean;
+  typing: boolean;
+  cooldown: number | null;
+  ratelimit: number;
+  description: string;
+  clientPermissions?: string[] | string;
+  userPermissions?: string[] | string;
+  usage?: string[] | string;
+}
+
+export enum Categories {
+  Administration = "Administration",
+  Anime = "Anime",
+  Etc = "Etc",
+  Fun = "Fun",
+  Gambling = "Gambling",
+  Nsfw = "NSFW",
+  OwnerOnly = "Owner only",
+  Reactions = "Reactions",
+  Roles = "Roles",
+  ServerSettings = "Server settings",
+  Utility = "Utility",
+}
+
+export enum Channel {
+  Guild = "guild",
+}
+
+export default class Commands extends Component {
+  render() {
+    return commandslist.map((c) => (
+      <div className="bounds">
+        <h2 onClick={() => collapse(c[0] as any)} className="categoryTitle">
+          {c[0]}
+        </h2>
+        {c.map((com) => {
+          // @ts-ignore
+          if (Categories[com] || com === c[0] || ["etc"].includes(com)) return;
+          console.log(com);
+          // @ts-ignore
+          return com.map((cmd) => {
+            return (
+              <object
+                name={c[0].toString()}
+                style={{
+                  display: "none",
+                }}
+              >
+                <p className="command">
+                  {new JSONCommand(cmd as any).createOutput()}
+                </p>
+              </object>
+            );
+          });
+        })}
+      </div>
+    ));
+  }
+}
+
+function collapse(c: any) {
+  const doc = document.getElementsByName(c);
+  doc.forEach((e) =>
+    e.style.display !== "none"
+      ? (e.style.display = "none")
+      : (e.style.display = "inline")
+  );
+}
+
+class JSONCommand {
+  private id: string;
+  private readonly aliases: string[];
+  private channel: Channel | null;
+  private readonly description: string;
+  private ownerOnly: boolean;
+  private readonly userPermissions: string[] | string | undefined;
+  private readonly usage: string[] | string | undefined;
+  createOutput: () => JSX.Element;
+  constructor(props: Command) {
+    this.id = props.id;
+    this.aliases = props.aliases;
+    this.channel = props.channel;
+    this.description = props.description;
+    this.ownerOnly = props.ownerOnly;
+    this.usage = props.usage;
+    this.userPermissions = props.userPermissions;
+    this.createOutput = () => {
+      const perms = this.userPermissions
+        ? Array.isArray(this.userPermissions)
+          ? this.userPermissions.join("\u000A")
+          : this.userPermissions
+        : null;
+
+      return (
+        <div>
+          {this.id.toUpperCase()}
+          <p className="key">
+            Aliases:
+            {
+              <text className="value">
+                {Array.isArray(this.aliases)
+                  ? this.aliases.join(", ")
+                  : this.aliases}
+              </text>
+            }
+          </p>
+          <p className="key">
+            Description: <text className="value">{this.description}</text>
+          </p>
+
+          <p className="key">
+            Usage: <text className="value">{this.usage}</text>
+          </p>
+
+          {perms && (
+            <p className="key">
+              Required permissions: <text className="value">{perms}</text>
+            </p>
+          )}
+        </div>
+      );
+    };
+  }
+}
