c6efb51c11e4833d6e1a5506e0858873e7b6641d
Cata <11278743+cataclym@users.noreply.github.com>
2022-08-15 00:02:26 +0200

    Added search method, category toggles and logic to display correct cmds

--- src/Commands.svelte
+++ src/Commands.svelte
@@ -1,126 +1,194 @@
 <script lang="ts">
-    import {Button, ProgressRing, TextBox} from "fluent-svelte";
-    import "fluent-svelte/theme.css";
-    import commands from "./cmdlist.js"
+  import {TextBox} from "fluent-svelte";
+  import "fluent-svelte/theme.css";
+  import commands from "./cmdlist.js";
+  import search from "./methods/search";
+
+  let active = {};
+
+  let cats = Object.entries(commands.commands);
+
+  function searchbarOnInput(c, category) {
+    cats = search(c, category);
+  }
+
+  function resetCats() {
+    cats = Object.entries(commands.commands);
+    active = {};
+  }
 </script>
+
 <div class="w-10/12 text-gray-300 m-auto mt-10 mb-52 flow-root">
-        <div class="bgColor mb-10">
-            <TextBox type="search" placeholder="Search commands"/>
-        </div>
+  <div class="bgColor mb-10">
+    <TextBox
+      type="search"
+      placeholder="Search commands"
+      on:input={(c) => searchbarOnInput(c, (() => active)())}
+      on:reset={resetCats}
+      on:abort={resetCats}
+    />
+  </div>
 
-    {#each Object.entries(commands) as categories}
-        {#each categories[1] as category}
-            <div class="cmdCategory">
-                {category[0]}
-            </div>
-        {/each}
+<!--      <p class="whitespace-pre text-left">{JSON.stringify(Object.entries(commands.commands)-->
+<!--          .map(cm => cm[1]), null, 12)-->
+<!--      }</p>-->
+  <!--<br>-->
+  <!--    <p>{JSON.stringify(commands.commands-->
+  <!--        .map(cb => cb[1])-->
+  <!--        .flat())-->
+  <!--    }</p>-->
+
+  {#each Object.entries(commands) as categories}
+    {#each categories[1] as category}
+      <div
+        class={active[category[0]] ? "cmdCategoryActive" : "cmdCategory"}
+        on:click={() => {
+          if (active[category[0]]) {
+            if (Object.keys(active).length !== 1) {
+              resetCats();
+            }
+            active[category[0]] = !active[category[0]];
+            resetCats();
+          } else {
+            active[category[0]] = !active[category[0]];
+            cats = cats.filter((a) => a[1][0] === category[0]);
+            if (Object.keys(active).length !== 1) {
+              resetCats();
+            }
+          }
+        }}
+      >
+        {category[0]}
+      </div>
     {/each}
+  {/each}
 
-    <div class="w-full m-auto mt-10 mb-5 flex">
-        <div class="cmd">
-            <p class="description">Command</p>
-        </div>
-        <div class="cmdDesc">
-            <p class="description">Description</p>
-        </div>
-        <div class="cmdUsage">
-            <p class="description">Usage</p>
-        </div>
+  <div class="w-full m-auto mt-10 mb-5 flex">
+    <div class="cmd">
+      <p class="description">Command</p>
     </div>
+    <div class="cmdDesc">
+      <p class="description">Description</p>
+    </div>
+    <div class="cmdUsage">
+      <p class="description">Usage</p>
+    </div>
+  </div>
 
-    {#each Object.entries(commands) as categories}
-        {#each categories[1] as category}
-            {#each category[1] as cmd}
-                <div class="m-auto flex mb-1 cmdContainer">
-                    <div class="cmd">
-                            +{cmd.id}
-                        <p class="categoryTxt">
-                            {category[0]}
-                        </p>
-                    </div>
-                    <div class="cmdDesc">
-                        <p class="description">
-                            {cmd.description}
-                    </div>
-                    <div class="cmdUsage">
-                        <p class="description">
-                            +{cmd.id} {Array.isArray(cmd.usage) ? cmd.usage.join(`\n+${cmd.id} `) : cmd.usage || ""}
-                        </p>
-                    </div>
-                </div>
-            {/each}
+  {#each cats as category}
+    {#each category[1] as group}
+      {#if typeof group !== "string"}
+        {#each group as cmd}
+          <div class="m-auto flex mb-1 cmdContainer">
+            <div class="cmd">
+              +{cmd.id}
+              <p class="categoryTxt">
+                {category[1][0]}
+              </p>
+            </div>
+            <div class="cmdDesc">
+              <p class="description">
+                {cmd.description}
+              </p>
+            </div>
+            <div class="cmdUsage">
+              <p class="description">
+                +{cmd.id}
+                {Array.isArray(cmd.usage)
+                  ? cmd.usage.join(`\n+${cmd.id} `)
+                  : cmd.usage || ""}
+              </p>
+            </div>
+          </div>
         {/each}
+      {/if}
     {/each}
+  {/each}
 </div>
+
 <style>
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 
-    .bgColor {
-        background-color: #1f1f1f;
-    }
+  .bgColor {
+    background-color: #1f1f1f;
+  }
 
-    .cmdCategory {
-        color: rgb(156 163 175);
-        margin: 0.5rem 0.5rem;
-        font-size: xx-large;
-        display: inline-flex;
-        padding: 0.2rem 0.5rem;
-        border: #1f1f1f 2px solid;
-        background-color: #1f1f1f;
-    }
+  .cmdCategory {
+    color: rgb(156 163 175);
+    margin: 0.5rem 0.5rem;
+    font-size: xx-large;
+    display: inline-flex;
+    padding: 0.2rem 0.5rem;
+    border: #1f1f1f 2px solid;
+    background-color: #1f1f1f;
+  }
 
-    .cmd {
-        padding-top: 1rem;
-        width: 33.3333%;
-        background-color: #353535;
-        vertical-align: middle;
-        display: inline;
+  .cmdCategory:hover {
+    border-bottom-color: rgb(234 88 12);
+    background-color: #0a0a0aff;
+    cursor: pointer;
+  }
 
-    }
-    .cmdDesc {
-        padding-top: 1rem;
-        width: 33.3333%;
-        background-color: #1f1f1f;
-        vertical-align: middle;
-        display: inline;
-    }
-    .cmdUsage {
-        padding-top: 1rem;
-        width: 33.3333%;
-        background-color: #353535;
-        vertical-align: middle;
-        display: inline;
-    }
+  .cmdCategoryActive {
+    color: #0a0a0aff;
+    margin: 0.5rem 0.5rem;
+    font-size: xx-large;
+    display: inline-flex;
+    padding: 0.2rem 0.5rem;
+    border: #1f1f1f 2px solid;
+    background-color: rgb(234 88 12);
+  }
 
-    .cmdButton {
-        color: rgb(156 163 175);
-        margin: 0.2rem 0.5rem;
-        padding: 0.2rem 0.5rem;
-        border: #1f1f1f 2px solid;
-        background-color: #1f1f1f;
-    }
+  .cmdCategoryActive:hover {
+    border-bottom: 2px solid black;
+    cursor: pointer;
+  }
 
-    .description {
-        max-width: 90%;
-        white-space: pre-wrap;
-        overflow: hidden;
-        margin: auto auto 1rem;
-    }
+  .cmd {
+    padding-top: 1rem;
+    width: 33.3333%;
+    background-color: #353535;
+    vertical-align: middle;
+    display: inline;
+  }
+  .cmdDesc {
+    padding-top: 1rem;
+    width: 33.3333%;
+    background-color: #1f1f1f;
+    vertical-align: middle;
+    display: inline;
+  }
+  .cmdUsage {
+    padding-top: 1rem;
+    width: 33.3333%;
+    background-color: #353535;
+    vertical-align: middle;
+    display: inline;
+  }
 
-    .cmdCategory:hover {
-        border-bottom-color: rgb(234 88 12);
-        background-color: #0A0A0AFF;
-        cursor: pointer;
-    }
+  .cmdButton {
+    color: rgb(156 163 175);
+    margin: 0.2rem 0.5rem;
+    padding: 0.2rem 0.5rem;
+    border: #1f1f1f 2px solid;
+    background-color: #1f1f1f;
+  }
 
-    .cmdContainer {
-        max-height: 10rem;
-        width: 100%;
-    }
+  .description {
+    max-width: 90%;
+    white-space: pre-wrap;
+    overflow: hidden;
+    margin: auto auto 1rem;
+  }
 
-    .categoryTxt {
-        font-size: small;
-    }
+  .cmdContainer {
+    max-height: 10rem;
+    width: 100%;
+  }
+
+  .categoryTxt {
+    font-size: small;
+  }
 </style>
---
+++ src/methods/search.ts
@@ -0,0 +1,31 @@
+import commands from "../cmdlist.js";
+
+const cats = Object.entries(commands.commands);
+
+export default function search(input, category: { [key: string]: true }) {
+  const inputText = input.originalTarget.value.toLowerCase().trim();
+
+  let mapped = cats.map((cm) => cm[1]);
+
+  if (Object.keys(category).length) {
+    const cat = Object.keys(category).shift();
+    mapped = mapped.filter((a) => a[0] === cat);
+  }
+
+  const filtered = mapped
+    .map((cb) => cb[1])
+    .flat()
+    .filter((a: { id: string; aliases: any[] }) =>
+      a.id.toLowerCase().includes(inputText)
+    );
+
+  return filtered
+    .map((res, i) => {
+      const cat = cats
+        .map((a) => a[1])
+        .find((a) => a[1].find((b) => b.id === res.id));
+      if (!cat || !cat[0]) return;
+      return [cats.findIndex((a) => a[0] === cat[0]), [cat[0], [res]]];
+    })
+    .filter(Boolean);
+}
