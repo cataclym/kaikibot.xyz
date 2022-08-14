4dd934c924571571788480f09be9083ecc9445e7
Cata <11278743+cataclym@users.noreply.github.com>
2022-08-15 00:02:41 +0200

    Prettier generated

--- src/ICmds.ts
+++ src/ICmds.ts
@@ -1,35 +1,223 @@
 export interface Command {
-    id: string;
-    categoryID: Categories;
-    category: string[];
-    filepath: string;
-    aliases: string[];
-    channel: Channel | null;
-    ownerOnly: boolean;
-    editable: boolean;
-    typing: boolean;
-    cooldown: number | null;
-    ratelimit: number;
-    description: string;
-    clientPermissions?: string[] | string;
-    userPermissions?: string;
-    usage?: string[] | string;
+  id: string;
+  categoryID: Categories;
+  category: string[];
+  filepath: string;
+  aliases: string[];
+  channel: Channel | null;
+  ownerOnly: boolean;
+  editable: boolean;
+  typing: boolean;
+  cooldown: number | null;
+  ratelimit: number;
+  description: string;
+  clientPermissions?: string[] | string;
+  userPermissions?: string;
+  usage?: string[] | string;
 }
 
 export enum Categories {
-    Administration = "Administration",
-    Anime = "Anime",
-    Etc = "Etc",
-    Fun = "Fun",
-    Gambling = "Gambling",
-    Nsfw = "NSFW",
-    OwnerOnly = "Owner only",
-    Reactions = "Reactions",
-    Roles = "Roles",
-    ServerSettings = "Server settings",
-    Utility = "Utility",
+  Administration = "Administration",
+  Anime = "Anime",
+  Etc = "Etc",
+  Fun = "Fun",
+  Gambling = "Gambling",
+  Nsfw = "NSFW",
+  OwnerOnly = "Owner only",
+  Reactions = "Reactions",
+  Roles = "Roles",
+  ServerSettings = "Server settings",
+  Utility = "Utility",
 }
 
 export enum Channel {
-    Guild = "guild",
+  Guild = "guild",
 }
+
+export type Cmds = {
+  commands: (
+    | (
+        | string
+        | [
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            }
+          ]
+      )[]
+    | (
+        | string
+        | [
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            }
+          ]
+      )[]
+    | (
+        | string
+        | [
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string[];
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            }
+          ]
+      )[]
+    | (
+        | string
+        | [
+            {
+              aliases: any[];
+              ownerOnly: boolean;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: any[];
+              ownerOnly: boolean;
+              channel: string;
+              description: string;
+              id: string;
+              userPermissions: string;
+            }
+          ]
+      )[]
+    | (
+        | string
+        | [
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string;
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            },
+            {
+              aliases: string[];
+              ownerOnly: boolean;
+              usage: string[];
+              channel: null;
+              description: string;
+              id: string;
+              userPermissions: string;
+            }
+          ]
+      )[]
+  )[];
+};
