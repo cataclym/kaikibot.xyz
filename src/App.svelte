7e86b502b40be817c06e2ea6b7a66ceec6c19e7d
Cata <cata@cata.moe>
2023-09-08 17:34:11 +0200

    mapped all color accents

--- src/App.svelte
+++ src/App.svelte
@@ -20,20 +20,20 @@
 </script>
 
 <main>
-  <h1 class="text-8xl mt-10 mb-5 font-bold text-orange-600">KAIKIBOT</h1>
+  <h1 class="text-8xl mt-10 mb-5 font-bold text-accent1">KAIKIBOT</h1>
   <h2 class="text-2xl mt-5 mb-10 text-gray-400">
     Your <mark>dad</mark> isn't <em>this</em> cool
   </h2>
   <div class="flex-auto">
     <a href={links.discord}>
       <button
-        class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 custom-width-1_7"
+        class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 custom-width-1_7"
         >SUPPORT SERVER
       </button>
     </a>
     <!--    -->
     <button
-            class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 custom-width-1_7"
+            class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 custom-width-1_7"
             on:click={() => (isActive = !isActive)}
     >
       COMMANDS
@@ -41,33 +41,33 @@
     <!--    </a>-->
     <a href={links.embed}>
       <button
-        class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 custom-width-1_7"
+        class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 custom-width-1_7"
         >EMBED BUILDER
       </button>
     </a>
     <a href={links.invite}>
       <button
-        class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 custom-width-1_7"
+        class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 custom-width-1_7"
         >INVITE KAIKI
       </button>
     </a>
     <a href={links.source}>
       <button
-        class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 gitlab custom-width-1_7"
+        class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 gitlab custom-width-1_7"
       >
         SOURCE CODE
       </button></a
     >
     <a href={links.paypal}>
       <button
-        class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 paypal custom-width-1_7"
+        class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 paypal custom-width-1_7"
       >
         DONATE
       </button>
     </a>
     <a href={links.patreon}>
       <button
-        class="h-16 whitespace-nowrap md:h-20 border-b-2 border-b-orange-600 text-gray-400 patreon custom-width-1_7"
+        class="h-16 whitespace-nowrap md:h-20 border-b-2  text-accent1 patreon custom-width-1_7"
       >
         PATREON
       </button>
@@ -81,7 +81,7 @@
         <h3 class="text-gray-400 font-bold text-2xl">
           Kaiki is a <mark class="italic">dad</mark>.
         </h3>
-        <h4 class="text-orange-600">
+        <h4 class="text-accent1">
           He will respond, like a dad, whenever you declare yourself with the
           use of 'I am'.
         </h4>
@@ -96,7 +96,7 @@
         <h3 class="text-gray-400 font-bold text-2xl">
           Useful, fun and silly commands
         </h3>
-        <h4 class="text-orange-600">
+        <h4 class="text-accent1">
           A wide variety of commands from fun and goofy commands to utility and
           server management focused commands.
         </h4>
@@ -111,7 +111,7 @@
         <h3 class="text-gray-400 font-bold text-2xl">
           Manage a large todo list
         </h3>
-        <h4 class="text-orange-600">
+        <h4 class="text-accent1">
           Create goals, save a link you just found or keep a tab on your loaning
           business.
         </h4>
@@ -124,7 +124,7 @@
     <div class="max-w-full md:flex flex-row-reverse justify-evenly mb-24">
       <div class="text-center md:w-96">
         <h3 class="text-gray-400 font-bold text-2xl">Economy and gambling</h3>
-        <h4 class="text-orange-600">
+        <h4 class="text-accent1">
           Spend your hard earned yen on the casino and compete in the currency
           leaderboard!
         </h4>
@@ -139,7 +139,7 @@
 <!--        <h3 class="text-gray-400 font-bold text-2xl">-->
 <!--          Kaiki is a <mark class="italic">dad</mark>.-->
 <!--        </h3>-->
-<!--        <h4 class="text-orange-600">-->
+<!--        <h4 class="text-accent1">-->
 <!--          He will respond, like a dad, whenever you declare yourself with the-->
 <!--          use of 'I am'.-->
 <!--        </h4>-->
@@ -148,7 +148,7 @@
 <!--        <h3 class="text-gray-400 font-bold text-2xl">-->
 <!--          Useful, fun and silly commands-->
 <!--        </h3>-->
-<!--        <h4 class="text-orange-600">-->
+<!--        <h4 class="text-accent1">-->
 <!--          A wide variety of commands from fun and goofy commands to utility and-->
 <!--          server management focused commands.-->
 <!--        </h4>-->
@@ -167,14 +167,14 @@
 <!--        <h3 class="text-gray-400 font-bold text-2xl">-->
 <!--          Manage a large todo list-->
 <!--        </h3>-->
-<!--        <h4 class="text-orange-600">-->
+<!--        <h4 class="text-accent1">-->
 <!--          Create goals, save a link you just found or keep a tab on your loaning-->
 <!--          business.-->
 <!--        </h4>-->
 <!--      </div>-->
 <!--      <div class="text-center md:w-72 md:m-auto">-->
 <!--        <h3 class="text-gray-400 font-bold text-2xl">Economy and gambling</h3>-->
-<!--        <h4 class="text-orange-600">-->
+<!--        <h4 class="text-accent1">-->
 <!--          Spend your hard earned yen on the casino and compete in the currency-->
 <!--          leaderboard!-->
 <!--        </h4>-->
@@ -192,13 +192,13 @@
     <!-- Large bottom features -->
     <div class="text-left text-gray-400 md:w-8/12 m-auto mt-52">
       <h3 class="text-gray-400 font-bold text-2xl">Open source</h3>
-      <h4 class="text-orange-600">
+      <h4 class="text-accent1">
         The codebase is transparent and available on GitLab. Fork it, clone it and contribute!
       </h4>
     </div>
     <div class="text-right text-gray-400 md:w-8/12 m-auto mt-10 mb-52">
       <h3 class="text-gray-400 font-bold text-2xl">Donations</h3>
-      <h4 class="text-orange-600">
+      <h4 class="text-accent1">
         KaikiBot is completely free and open source. Donations are completely optional, but highly appreciated.<br>Donations will go towards server costs and support the developer(s), as well as give you reward through Patreon on the Discord server.
       </h4>
     </div>
@@ -208,10 +208,10 @@
   </div>
 
   <div
-    class="grid m-auto mb-20 mt-10 grid-cols-3 justify-items-center w-8/12 2xl:w-1/2 text-gray-400"
+    class="grid m-auto mb-20 mt-10 grid-cols-3 justify-items-center w-8/12 2xl:w-1/2 text-accent1"
   >
     <div class="grid">
-      <h3 class="mt-1 text-orange-600 text-xl text-left">Kaiki</h3>
+      <h3 class="mt-1 text-accent1 text-xl text-left">Kaiki</h3>
       <h3 class="mt-1 flex-col text-left"><a href={links.invite}>Invite</a></h3>
       <h3
         class="mt-1 flex-col text-left cursor-pointer"
@@ -221,12 +221,12 @@
       </h3>
     </div>
     <div class="grid">
-      <h2 class="text-orange-600 text-xl text-left">Selfhosting</h2>
+      <h2 class="text-accent1 text-xl text-left">Selfhosting</h2>
       <h3 class="flex-col text-left"><a href={links.source}>Source code</a></h3>
       <h3 class="flex-col text-left"> <a href={links.docs}>Docs</a></h3>
     </div>
     <div class="grid">
-      <h2 class="text-orange-600 text-xl text-left">Utilities</h2>
+      <h2 class="text-accent1 text-xl text-left">Utilities</h2>
       <h3 class="flex-col text-left"> <a href={links.changelog}>Changelog </a></h3>
     </div>
   </div>
@@ -253,22 +253,47 @@
   @tailwind utilities;
 
   :root {
-    --tw-text-opacity: 1;
-    --orange: rgb(234 88 12 / var(--tw-text-opacity));
+    --background: #252422;
+    --accent1: #CCC5B9;
+    --accent2: #403D39;
+    --accent3: #FFFCF2;
+    --accent4: #E85E28;
+
+    --gitlab: #6b4fbb;
+    --paypal: #003087;
+    --patreon: #ff424d;
   }
 
   :global(body) {
-    background-color: #0a0a0aff;
+    background-color: var(--background);
     padding: 1px !important;
     overflow-y: auto; /* vertical scrollbar */
     overflow-x: hidden; /* horizontal scrollbar */
   }
+
   main {
     text-align: center;
     padding: 0.01em;
     max-width: none;
     margin: 0 auto;
   }
+
+  .text-accent1 {
+    color: var(--accent1)
+  }
+
+  .text-accent2 {
+    color: var(--accent2)
+  }
+
+  .text-accent3 {
+    color: var(--accent3)
+  }
+
+  .text-accent4 {
+    color: var(--accent4)
+  }
+
 
   .feature-block {
     margin-top: 3em;
@@ -286,8 +311,7 @@
   }
 
   mark {
-    --tw-text-opacity: 1;
-    color: rgb(234 88 12 / var(--tw-text-opacity));
+    color: var(--accent4);
     background-color: inherit;
   }
 
@@ -311,34 +335,68 @@
     /*font-size: 1.5em;*/
     /*text-align: center;*/
     /*width: 14.92em;*/
-    border-top: #1f1f1f solid 2px;
-    border-right: #1f1f1f solid 2px;
-    border-left: #1f1f1f solid 2px;
-    background-color: #1f1f1f;
-    /*border: 0 solid;*/
+    /*border-top: var(--accent2) solid 2px;*/
+    /*border-right: var(--accent2) solid 2px;*/
+    /*border-left: var(--accent2) solid 2px;*/
+    background-color: var(--accent2);
+    border: var(--accent2) 2px solid;
+    color: var(--accent1) !important;
+    text-shadow: var(--background) 2px !important;
+    transition: 0.5s;
     /*border-bottom: 4px solid var(--orange);*/
   }
 
   button:hover {
     cursor: pointer;
-    background-color: #0a0a0aff;
+    background-color: var(--background);
+    color: var(--accent1) !important;
+    border-color: var(--accent4);
+    transform: scale(105%);
+    transition: 0.5s;
   }
 
-  .gitlab {
-    border-bottom-color: #6b4fbb;
+  .gitlab:hover {
+    /*border-bottom-color: var(--gitlab);*/
   }
 
-  .patreon {
-    border-bottom-color: #ff424d;
+  .patreon:hover {
+    /*border-bottom-color: var(--patreon);*/
   }
 
-  .paypal {
-    border-bottom-color: #003087;
+  .paypal:hover {
+    /*border-bottom-color: var(--paypal);*/
+  }
+
+  @keyframes gitlabColor {
+    from {
+
+    }
+    to {
+      transform: translateY(1em);
+    }
+  }
+
+  @keyframes paypalColor {
+    from {
+      background-color: var(--background);
+    }
+    to {
+      background-color: var(--paypal);
+    }
+  }
+
+  @keyframes patreonbColor {
+    from {
+      background-color: var(--background);
+    }
+    to {
+      background-color: var(--patreon);
+    }
   }
 
   footer {
-    color: #999999;
-    background: #1f1f1f;
+    color: var(--accent3);
+    background: var(--accent2);
     width: 100%;
     height: 3em;
     padding: 0.7% 0 0 0;
--- src/Placeholders.svelte
+++ src/Placeholders.svelte
@@ -1,10 +1,10 @@
 <svg width="24rem" height="15rem" class="svg-class">
-    <rect style="fill: rgba(234,88,12,0.5)" width="24rem" height="15rem"/>
-    <text dx="50%" text-anchor="middle" dy="50%" font-size="64" fill="#1f1f1f">Placeholder</text>
+    <rect style="fill: var(--accent2)" width="24rem" height="15rem"/>
+    <text dx="50%" text-anchor="middle" dy="60%" font-size="64" fill="var(--background)">Placeholder</text>
 </svg>
 
 <style>
     .svg-class:hover {
-        background-color: rgba(234,88,12,1);
+        background-color: var(--accent4);
     }
 </style>
