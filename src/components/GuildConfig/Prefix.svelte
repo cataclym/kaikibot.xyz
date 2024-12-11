ce934aee018eb8ac862858db62b7e4a2291613d2
Cata <cata@cata.moe>
2024-12-11 15:26:48 +0100

    Updated deps

---
+++ src/components/GuildConfig/Prefix.svelte
@@ -0,0 +1,15 @@
+		<div class="indent flex flex-col items-center justify-between">
+			<h3>Server prefix</h3>
+			<Input
+				id="prefix"
+				type="text"
+				placeholder={Prefix}
+				bind:value={Prefix}
+				style="width: 4rem; border-radius: 0.5rem;"
+			/>
+			<Button
+				color="primary"
+				class="self-end ml-auto mr-auto enabled:cursor-pointer border-transparent"
+				disabled={prefixState}><FileCheckSolid />Save</Button
+			>
+		</div>
